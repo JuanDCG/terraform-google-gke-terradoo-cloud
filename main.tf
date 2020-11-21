8018fd619c77be116090235ea1644a37002a9c02
JuanDCG <juandcg314@gmail.com>
2020-11-21 17:57:55 +0100

    change version

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 terraform {
-  # The modules used in this example have been updated with 0.13 syntax, additionally we depend on a bug fixed in
-  # version 0.13.5.
-  required_version = ">= 0.13.5"
+  # The modules used in this example have been updated with 0.12 syntax, additionally we depend on a bug fixed in
+  # version 0.12.26.
+  required_version = ">= 0.12.26"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
